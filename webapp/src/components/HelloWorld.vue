8347c3f8868c710b0226f3bfbeeee538c0220874
yan160100 <1909484942@qq.com>
2023-03-08 16:42:16 +0800

    添加element plus

--- webapp/src/components/HelloWorld.vue
+++ webapp/src/components/HelloWorld.vue
@@ -1,13 +1,13 @@
 <script setup>
-import { reactive } from 'vue'
+import { ref, reactive } from 'vue'
 
+const input = ref('')
 const arr = reactive([])
 
 const ws = new WebSocket('ws://localhost:2021');
 console.log(ws)
 
 ws.onopen = (e) => {
-  ws.send("233")
 }
 
 ws.onmessage = e => {
@@ -19,10 +19,17 @@ ws.close = e => {
   console.log('关闭连接');
 }
 
+function send() {
+  ws.send(input.value)
+}
 </script>
 
 <template>
   <p style="text-align:center">{{ arr }}</p>
+  <p style="text-align:center">
+    <el-input v-model="input" placeholder="Please input" clearable style="width: 250px" />
+    <el-button type="primary" @click="send">发 送</el-button>
+  </p>
 </template>
 
 <style scoped>
