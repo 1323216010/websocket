e456d510665049a70ec3eeff988d0aba8adf48a5
yan160100 <1909484942@qq.com>
2023-03-08 11:13:23 +0800

    添加ws

--- js-server/main.js
+++ js-server/main.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import arr from './json/poet300.json' assert {type: 'json'}
+import { WebSocketServer } from 'ws';
 
 var app = express()
 
@@ -18,3 +19,15 @@ var server = app.listen(8081, function () {
     var port = server.address().port
     console.log("访问端口为%s", port)
 })
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on('connection', function connection(ws) {
+  ws.on('error', console.error);
+
+  ws.on('message', function message(data) {
+    console.log('received: %s', data);
+  });
+
+  ws.send('something');
+});
