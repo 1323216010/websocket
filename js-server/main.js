5606707596968fec109105e28d3528be3b16ee21
yan160100 <1909484942@qq.com>
2023-03-13 15:40:48 +0800

    通过接口控制服务端主动发消息给指定客户端

--- js-server/main.js
+++ js-server/main.js
@@ -1,11 +1,35 @@
-import { WebSocketServer } from 'ws';
+import WebSocket, { WebSocketServer } from 'ws';
+import express from 'express'
 
 const wss = new WebSocketServer({ port: 2021 });
 
+var i = 0
 wss.on('connection', function connection(ws) {
-  ws.on('error', console.error);
-
+  ws.on('error', console.error)
+  ws["id"] = i
+  i++
   ws.on('message', function message(data) {
-    ws.send(String(data));
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(String(data));
+      }
+    });
   });
 });
+
+var app = express()
+
+app.get('/send', function (req, res) {
+
+  wss.clients.forEach(function each(client) {
+    if (client.readyState === WebSocket.OPEN && client.id == req.query.id) {
+      client.send(req.query.msg);
+    }
+  })
+  
+})
+
+var server = app.listen(8081, function () {
+  var port = server.address().port
+  console.log("访问端口为%s", port)
+})
