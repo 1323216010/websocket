6ca5cf1e6b760c35d799e63bc813dc32911d67d6
yan160100 <1909484942@qq.com>
2023-03-17 17:34:38 +0800

    添加fastapi

--- python-server/main.py
+++ python-server/main.py
@@ -1,12 +1,25 @@
 import asyncio
 import websockets
+from fastapi import FastAPI, Request
+import uvicorn
+
+list = []
 
 async def echo(websocket):
-    async for message in websocket:
-        await websocket.send(message)
+    msg = await websocket.recv()
+    await websocket.send(msg)
+    list.append(websocket)
 
 async def main():
-    async with websockets.serve(echo, "localhost", 2024):
+    async with websockets.serve(echo, "localhost", 2021):
         await asyncio.Future()  # run forever
 
 asyncio.run(main())
+
+app = FastAPI()
+@app.get("/send")
+async def send(request: Request):
+    list[request.query_params.get("id")].send(request.query_params.get("msg"))
+
+if __name__ == "__main__":
+    uvicorn.run(app="main:app", port=8084, reload=True)
