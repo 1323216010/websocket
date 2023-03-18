cd9ed437d577a8c7d999310b8ef60ba9e28d9314
13232160100 <ypc0100@qq.com>
2023-03-18 16:56:24 +0800

    添加依赖文件

---
+++ python-server/README.md
@@ -0,0 +1,3 @@
+virtualenv venv --python=C:\Users\yan\AppData\Local\Programs\Python\Python39\python.exe
+pip freeze >requirements.txt
+pip install - r requirements.txt
--- python-server/main.py
+++ python-server/main.py
@@ -1,8 +1,9 @@
 import asyncio
+import multiprocessing
+
 import websockets
 from fastapi import FastAPI, Request
 import uvicorn
-from multiprocessing import Process
 
 list = []
 
@@ -10,28 +11,28 @@ async def echo(websocket):
     msg = await websocket.recv()
     await websocket.send(msg)
     list.append(websocket)
-    print(list)
 
-async def main():
+async def ws(list):
     async with websockets.serve(echo, "localhost", 2021):
         await asyncio.Future()  # run forever
 
 app = FastAPI()
 @app.get("/send")
 async def send(request: Request):
-    print(list)
     list[int(request.query_params.get("id"))].send(request.query_params.get("msg"))
-def ws():
-    asyncio.run(main())
 
-def h():
+def f(list):
+    asyncio.run(ws(list))
+
+def g(list):
     uvicorn.run(app="main:app", port=8084, reload=True)
 
-def f(name):
-    print('hello', name)
-
 if __name__ == '__main__':
-    p1 = Process(target=ws)
-    p2 = Process(target=h)
+    list1 = multiprocessing.Array('i', list)
+    print(list1[:])
+    p1 = multiprocessing.Process(target=f, args=(list1,))
+    p2 = multiprocessing.Process(target=g, args=(list1,))
     p1.start()
     p2.start()
+    p1.join()
+    p2.join()
